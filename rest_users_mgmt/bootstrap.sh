15b1b6f9e7163d3a62952ec21aa6f9143660ec46
phenix <rajpurohitmangu@gmail.com>
2018-02-27 21:16:51 +0530

    Added supervisor file

---
+++ rest_users_mgmt/bootstrap.sh
@@ -0,0 +1,8 @@
+#whereis source
+#source /tmp/tmp1/hb_users_api/rest_users_mgmt/docker_venv/bin/activate
+supervisord -c supervisord.conf
+#supervisorctl reread 
+#supervisorctl update
+#python manage.py runserver localhost:7777 &
+service rabbitmq-server start
+celery -A rest_users_mgmt worker -l info 
